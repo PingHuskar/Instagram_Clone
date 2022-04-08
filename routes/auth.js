c829e2a166550f2a8f2275bd2c14fb96fa48f2b2
PingHuskar <61967318+PingHuskar@users.noreply.github.com>
2022-04-08 12:27:45 +0700

    post data to mongodb

--- routes/auth.js
+++ routes/auth.js
@@ -2,6 +2,9 @@ const express = require('express');
 
 const router = express.Router()
 
+const mongoose = require('mongoose');
+const User = mongoose.model("User")
+
 router.get('/',(req,res)=>{
 	res.send("hello")
 })
@@ -11,9 +14,28 @@ router.post('/signup',(req,res)=>{
 	if (!email || !password || !name) {
 		res.json({error:"Field Empty"})
 	}
-	else {
-		res.json({message:"Success"})
-	}
+	// else {
+	// 	res.json({message:"Success"})
+	// }
+	User.findOne({email:email})
+	.then((savedUser)=> {
+		if (savedUser) {
+			return res.status(422).json({error:"Email Already Exist"})
+		}
+		const user = new User({
+			email,password,name
+		})
+		user.save()
+		.then(user => {
+			res.json({message:"saved Success"})
+		})
+		.catch(err => {
+			console.log(err)
+		})
+	})
+	.catch(err => {
+			console.log(err)
+		})
 })
 
 module.exports = router
