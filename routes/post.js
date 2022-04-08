6ebfcff536c79b2e205bae480fb9874ce8ce401c
PingHuskar <61967318+PingHuskar@users.noreply.github.com>
2022-04-08 19:30:30 +0700

    add mypost route

--- routes/post.js
+++ routes/post.js
@@ -3,14 +3,27 @@ const router = express.Router()
 const mongoose = require('mongoose');
 const requireLogin = require('../middleware/requireLogin') 
 const Post = mongoose.model("Post")
+
+router.get('/allpost',(req,res)=>{
+    Post.find()
+    .populate("postedBy","_id name")
+    .then(posts => {
+        res.json({posts})
+    })
+    .catch(err=>{
+        console.log(err)
+   })
+})
+
 router.post('/createpost',requireLogin, (req,res)=>{
-    const {title,body} = req.body
-    if(!title || !body){
+    const {title,body,photo} = req.body
+    if(!title || !body || !photo){
       return res.status(422).json({error:"Plase add all the fields"})
     }
     const post = new Post ({
         title,
         body,
+        photo,
         postedBy:req.user
     })
     post.save().then(result=>{
@@ -18,6 +31,17 @@ router.post('/createpost',requireLogin, (req,res)=>{
     })
     .catch(err=>{
          console.log(err)
+    })
+})
+
+router.get('/mypost',requireLogin,(req,res)=>{
+    Post.find({postedBy:req.user._id})
+    .populate("postedBy","_id name")
+    .then(mypost=>{
+        res.json({mypost})
+    })
+    .catch(err=>{
+        console.log(err)
     })
 })
 
